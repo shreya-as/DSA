fd707c907793a4e70aa669c7dbae38ef2fcd1d15
shreya-as <shreyathakuri1372@gmail.com>
2024-11-12 22:37:02 +0545

    add commit

--- sorting/BubbleSort.js
+++ sorting/BubbleSort.js
@@ -1,8 +1,8 @@
 arr = [10, 23, 1, 34, 2];
 let temp;
 for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length - 1; j++) {
-    // When we reach the last index, there are no more elements to compare
+  for (let j = 0; j < arr.length - 1; j++) { // When we reach the last index, there are no more elements to compare
+
     if (arr[j] > arr[j + 1]) {
       //   temp = arr[j + 1];
       //   arr[j + 1] = arr[j];
