61d9f4833811f6096d39853c834f4e8e347070ef
shreya-as <shreyathakuri1372@gmail.com>
2024-11-12 22:36:36 +0545

    fix: use destructuring to swap the elements in array

--- sorting/BubbleSort.js
+++ sorting/BubbleSort.js
@@ -1,12 +1,15 @@
 arr = [10, 23, 1, 34, 2];
 let temp;
-for (let i = 0; i < arr.length ; i++) {
-  for (let j = 0; j < arr.length ; j++) {
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - 1; j++) {
+    // When we reach the last index, there are no more elements to compare
     if (arr[j] > arr[j + 1]) {
-      temp = arr[j + 1];
-      arr[j + 1] = arr[j];
-      arr[j] = temp;
+      //   temp = arr[j + 1];
+      //   arr[j + 1] = arr[j];
+      //   arr[j] = temp;
+      // Use destructuring for swapping elements
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
   }
 }
-console.log(arr,"arrarr")
+console.log(arr, "arrarr");
