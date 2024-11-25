1a66c749ac8976c148705a41abbeeecd561a3c28
shreya-as <shreyathakuri1372@gmail.com>
2024-11-25 21:57:19 +0545

    solved recursion problem

---
+++ Recursion/array.js
@@ -0,0 +1,23 @@
+const multiply = (arr) => {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
+};
+const arr = [1, 2, 3, 4];
+console.log(multiply(arr));
+
+// using recursion
+const multiplyWithRecursion = (arr) => {
+  if (arr.length === 0) {
+    return 1;
+  }
+  console.log(arr, "arrrrr");
+  return arr[0] * multiplyWithRecursion(arr.slice(1));
+  // 1 * multiplyWithRecursion([2,3,4]);
+  //1* 2 * multiplyWithRecursion([3,4]);
+  // 1*2*3 * multiplyWithRecursion([4]);
+  // 1*2*3*4
+};
+console.log(multiplyWithRecursion(arr));
