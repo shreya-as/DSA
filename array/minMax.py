74f7c746d2073d98076a0369512868646569db44
shreya-as <shreyathakuri1372@gmail.com>
2024-12-06 13:55:33 +0545

    update the min max values using min max method

--- array/minMax.py
+++ array/minMax.py
@@ -1,12 +1,14 @@
 def minMax(arr):
     print(arr,"arr")
-    min=arr[0]
-    max=arr[0]
+    min_val=arr[0]
+    max_val=arr[0]
     for x in range(len(arr)-1):
-        if(arr[x]>max):
-            max=arr[x]
-        if(arr[x]<min):
-            min=arr[x]
-    return [min,max]
+        # if(arr[x]>max):
+        #     max=arr[x]
+        # if(arr[x]<min):
+        #     min=arr[x]
+        min_val=min(min_val,arr[x])
+        max_val=max(max_val,arr[x])
+    return [min_val,max_val]
 
 print(minMax([3,1,60,100]))
