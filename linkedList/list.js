3c54f28b7feb77cb6a5c7b2565f9063e67b4aab8
shreya <shreyathakuri1372@gmail.com>
2024-12-22 22:08:55 +0545

    chore: add add to tail and add to head in linkedlist

--- linkedList/list.js
+++ linkedList/list.js
@@ -1,20 +1,39 @@
-// we have value property 
-// next node and prevnode 
+// we have value property
+// next node and  
 
-function LinkedList(){
-    this.head=null;
-    this.tail=null;
-    
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
 }
-function Node(value, next , prev) {
-this.value=value;
-this.next=next;
-this.prev= prev;
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+LinkedList.prototype.addToHead = function (value) {
+  var newNode = new Node(value, this.head, null);
+  if (this.head) this.head.prev = newNode;
+  else this.tail = newNode 
+  this.head=newNode
+};
 
+
+
+
+LinkedList.prototype.addToTail=function(value){
+var newNode=new Node(value,null,this.tail)
+if(this.tail) this.tail.next= newNode
+ else this.head=newNode
+ this.tail=newNode
 }
-var node1= new Node(100,"node2",null);
-console.log(node1);
-var LL =new LinkedList();
-var userList =new LinkedList();
-var toDoList = new LinkedList();
-console.log(LL)
+var ll = new LinkedList();
+ll.addToHead(100);
+console.log(ll)
+var myLl=new LinkedList();
+myLl.addToTail(10)
+myLl.addToTail(30)
+myLl.addToHead(900)
+
+ 
+console.log(myLl)
+console.log(myLl.tail.prev)
